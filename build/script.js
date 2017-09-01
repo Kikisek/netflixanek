ec8fa7a3adbc84cad483b21ff74032d90305a796
Kiki <kiki@example.com>
2017-09-01 08:30:45 -0400

    Move description of the site to the bottom, add layout of the search criteria

--- build/script.js
+++ build/script.js
@@ -5,17 +5,17 @@
 $(document).ready(function() {
   $.getJSON("https://netflix-csfd.herokuapp.com/movies", function(data){
   
-
     for (var movie of data) {
-      $("tbody").append("<tr><td class='title col-md-8'>" + movie.title + "</td>" +
-                        "<td class='rating col-md-1'>" + movie.rating + "%" + "</td>" +  //from movie.rating -> link to csfd
-                        "<td class='release year col-md-1'>" + movie.original_release_year + "</td>" +
-                        "<td class='gen col-md-1'><i class=\"" + genres[Math.floor(Math.random()*genres.length)] + " aria-hidden='true'\"></i></td></tr>" +
-                        "<tr class='description'><td colspan='4'>" + movie.short_description + "</td></tr>");
+      $("tbody").append(`
+        <tr><td class='title col-md-8'>${movie.title}</td>             
+        <td class='rating col-md-1'>${movie.rating + "%"}</td>            //link to csfd
+        <td class='release year col-md-1'>${movie.original_release_year}</td>
+        <td class='gen col-md-1'><i class='${genres[Math.floor(Math.random()*genres.length)]}' aria-hidden='true'></i></td></tr>
+        <tr class='description'><td colspan='4'>${movie.short_description}</td></tr>
+      `);
     };
 
     $("tr td.title").click (function (event) {
-      $(this).toggleClass("bold");
       $(this).parent().next(".description").slideToggle();
     });
     // $(".description").click(function(event){
