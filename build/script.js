1005a6cae326bbfdc7ca6a20ad71520860b6a587
Kiki <kiki@example.com>
2017-09-14 10:11:24 -0400

    Use faker to generate test data and fill filters with options

--- build/script.js
+++ build/script.js
@@ -1,23 +1,77 @@
+function render(data){
+  $("tbody").empty();
+  for (var movie of data) {
+    $("tbody").append(`
+      <tr><td class='title' colspan='3'>${movie.title}</td></tr>             
+      <tr class='details'><td class='rating'>${movie.rating_csfd + "%"}</td>            //link to csfd
+      <td class='releaseYear'>${movie.year}</td>
+      <td class='gen'>${movie.genres.join(", ")}</td></tr>
+      <tr class='description'><td colspan='3'><p>${movie.description}</p></td></tr>
+    `);
+  };
+};
 
-// data loaded
+function filterMovies (movies, year, country, genre, type, watched) {
+  return movies.filter(function(film){
+    var seen = true;
+    if (film.my_rating_csfd === null) {
+      seen = false;
+    }
+    var isWithinDecade = !year || (film.year > year && film.year < year + 10);
+    var isFromCountry = !country || film.origins.includes(country);
+    var isGenre = !genre || film.genres.includes(genre);
+    var isType = !type || film.type.toLowerCase() === type.toLowerCase();
+    var isWatched = seen === watched;
+    return isWithinDecade && isFromCountry && isGenre && isType && isWatched;
+  })
+}
+
+function extractCriteria(id){
+  if ($("#" + id)[0].selectedIndex === 0){
+    return null;
+  }
+  return $("#" + id).val();
+}
 
 $(document).ready(function() {
-  $.getJSON("https://netflix-csfd.herokuapp.com/movies", function(data){
+  for (var i = 1940; i <= (new Date().getFullYear()); i += 10){
+    $("#year").append("<option>" + i + "s</option>");
+  };
 
-    for (var movie of data) {
-      $("tbody").append(`
-        <tr><td class='title col-md-8'>${movie.title}</td>             
-        <td class='rating col-md-1'>${movie.rating + "%"}</td>            //link to csfd
-        <td class='releaseYear col-md-1'>${movie.original_release_year}</td>
-        <td class='gen col-md-1'><i class='${genres[Math.floor(Math.random()*genres.length)]}' aria-hidden='true'></i></td></tr>
-        <tr class='description'><td colspan='4'><p>${movie.short_description}</p></td></tr>
-      `);
-    };
+  for (var key in localization.countries){
+    $("#country").append("<option>" + key + "</option>");
+  };
 
-    $(".title").click (function (event) {
-      $(this).parent().next(".description").children().children().slideToggle();
-    });
+  for(var key in localization.genres){
+    $("#genre").append("<option>" + key + "</option>");
+  };
+
+  // $.getJSON("https://netflix-csfd.herokuapp.com/movies", function(allData){
+
+  // render(movies);
+
+  $(".title").click (function (event) {
+    $(this).parent().nextAll(".description:first").children().children().slideToggle();
+  });
+
+  $(".btn-success").click(function() {
+    var yearStr = extractCriteria("year");
+    var year = yearStr === null ? null : parseInt(yearStr);
+    var country = extractCriteria("country");
+    var genre = extractCriteria("genre");
+    var type = extractCriteria("type");
+    var watched = $("#watched").is(":checked"); //TBD
+    render(filterMovies(movies, year, country, genre, type, watched));
+  });
+
+  $(".btn-danger").click(function(){
+    $("#year")[0].selectedIndex = 0;
+    $("#country")[0].selectedIndex = 0;
+    $("#genre")[0].selectedIndex = 0;
+    $("#type")[0].selectedIndex = 0;
+    $('#watched').prop('checked', false); 
+  });
 
     $(".loader").addClass("hide");
   });
-});
+// });
