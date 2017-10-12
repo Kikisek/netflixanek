1b8ea8412adbd66a158a126d50683d7fb9ce6d4e
Kiki <kiki@example.com>
2017-10-12 17:44:42 -0400

    Change favicon, slight refactor

--- build/script.js
+++ build/script.js
@@ -1,27 +1,31 @@
-function render(data){
+// fill table with data
+function render (data) {
   $("tbody").empty();
   var rating;
   for (var movie of data) {
-    if (movie.rating_csfd === null){
+    if (movie.rating_csfd === null) {
       rating = "no rating";
     } else {
       rating = movie.rating_csfd + "%";
     }
     $("tbody").append(`
-      <tr class='openDescription'><td class='title' colspan='3'>${movie.title}</td></tr>             
-      <tr class='details openDescription'><td class='rating'>${rating}</td>
-      <td class='releaseYear'>${movie.year}</td>
-      <td class='gen'>${movie.genres.join(", ")}</td></tr>
+      <tr class='open-description'><td class='title' colspan='3'>${movie.title}</td></tr>             
+      <tr class='details open-description'>
+        <td class='rating'>${rating}</td>
+        <td class='releaseYear'>${movie.year}</td>
+        <td class='genres'>${movie.genres.join(", ")}</td>
+      </tr>
       <tr class='description'><td colspan='3'><p>${movie.description}</p></td></tr>
     `);
   };
-  $(".openDescription").click (function (event) {
+  $(".open-description").click (function (event) {
     $(this).nextAll(".description:first").children().children().slideToggle();
   });
 };
 
+// filter data
 function filterMovies (movies, year, country, genre, type, watched) {
-  return movies.filter(function(film){
+  return movies.filter(function (film) {
     var seen = true;
     if (film.my_rating_csfd === null) {
       seen = false;
@@ -35,51 +39,60 @@ function filterMovies (movies, year, country, genre, type, watched) {
   })
 }
 
-function extractCriteria(id){
+// extract search criteria from search options
+function extractCriteria(id) {
   if ($("#" + id)[0].selectedIndex === 0){
     return null;
   }
   return $("#" + id).val();
 }
 
+// load data from server
 $.getJSON("https://netflix-csfd.herokuapp.com/movies", function (data) {
+  // if there is no genre or origin, return empty array
   var movies = data.map(function(movie) {
     movie.genres = movie.genres ? movie.genres : [];
     movie.origins = movie.origins ? movie.origins : [];
     return movie;
   })
+
+  // load document
   $(document).ready(function() {
     //fill the filters with options
-    for (var i = 1940; i <= (new Date().getFullYear()); i += 10){
+    for (var i = 1940; i <= (new Date().getFullYear()); i += 10) {
       $("#year").append("<option>" + i + "s</option>");
     };
-    for (var key in localization.countries){
+    for (var key in localization.countries) {
       $("#country").append("<option>" + key + "</option>");
     };
-    for(var key in localization.genres){
+    for (var key in localization.genres) {
       $("#genre").append("<option>" + key + "</option>");
     };
 
+    // fill table with movies
     render(movies);
 
+    // filter movies
     $(".btn-success").click(function() {
       var yearStr = extractCriteria("year");
       var year = yearStr === null ? null : parseInt(yearStr);
       var country = extractCriteria("country");
       var genre = extractCriteria("genre");
       var type = extractCriteria("type");
-      var watched = $("#watched").is(":checked");
+      var watched = $("#custom-checkbox").is(":checked");
       render(filterMovies(movies, year, country, genre, type, watched));
     });
 
-    $(".btn-danger").click(function(){
+    // reset search options
+    $(".btn-danger").click(function() {
       $("#year")[0].selectedIndex = 0;
       $("#country")[0].selectedIndex = 0;
       $("#genre")[0].selectedIndex = 0;
       $("#type")[0].selectedIndex = 0;
-      $('#watched').prop('checked', false); 
+      $('#custom-checkbox').prop('checked', false); 
     });
 
+    // hide loader when data is loaded
     $(".loader").addClass("hide");
   });
 });
